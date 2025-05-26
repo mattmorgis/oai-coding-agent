bcb1378e9d2c58fb3ec270e6de2e6aef2daa644f
Matthew Morgis <matthew.morgis@gmail.com>
2025-05-26 00:26:38 -0400

    Bind Shift+Enter as Ctrl+J mapping for newline, remove CR/LF mapping

--- src/oai_coding_agent/console/key_bindings.py
+++ src/oai_coding_agent/console/key_bindings.py
@@ -15,4 +15,16 @@ def get_key_bindings() -> KeyBindings:
         else:
             buffer.complete_next()
 
+    # Support Shift+Enter (mapped to Ctrl+J in your terminal) for newline without submission.
+    @kb.add("c-j", eager=True)
+    def _(event):
+        """Insert newline on Ctrl+J (recommended Shift+Enter mapping in terminal)."""
+        event.current_buffer.insert_text("\n")
+
+    # Enter submits input
+    @kb.add(Keys.Enter, eager=True)
+    def _(event):
+        """Submit input on Enter"""
+        event.current_buffer.validate_and_handle()
+
     return kb
